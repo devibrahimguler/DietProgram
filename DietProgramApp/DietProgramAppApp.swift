356697a01c3b41ec257be6dae397a67d797ea843
muntazif <04190000633@ogrenci.ege.edu.tr>
2022-06-04 02:36:03 +0300

    Initial Commit

---
+++ DietProgramApp/DietProgramAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DietProgramAppApp.swift
+//  DietProgramApp
+//
+//  Created by İbrahim Güler on 4.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct DietProgramAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
